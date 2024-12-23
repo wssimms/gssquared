82d61e3483c499f61c888c0349d28b56e9a53412
Jawaid Bazyar <bazyar@gmail.com>
2024-12-22 19:46:18 -0500

    added lots of tests

--- test.cpp
+++ test.cpp
@@ -46,9 +46,18 @@ void assert_cycles(cpu_state *cpu, uint64_t expected_cycles) {
 void demo_ram() {
     Test test = {
         .program = {
-            OP_LDX_IMM, 0xD0,       // 2 cycles
+            OP_LDX_IMM, 0xFF, // init the stack pointer
+            OP_TXS_IMP,
+
+            OP_JSR_ABS, 0x09, 0x03, // JSR to next instruction (ldx imm hehe)
+            OP_JMP_ABS, 0x00, 0x03, // do the whole thing again
+
+            OP_LDX_IMM, 0xD0,       // 2 cycles  should be 0x0307
             OP_LDA_ZP_X, 0x00,      // 3 cycles
             OP_LDA_ZP_X, 0x40,      // 3 cycles
+
+            OP_STA_ZP_X, 0x15,      // 3 cycles
+            OP_STA_ABS_X, 0x12, 0x34, // 4 cycles
 
             OP_LDA_ABS, 0x12, 0x34, // 4 cycles
             OP_LDA_ABS_X, 0x12, 0x34, // 4 cycles?
@@ -123,7 +132,51 @@ void demo_ram() {
             OP_CLC_IMP,
             OP_ADC_ZP, 0x78,
 
+    // sbc immediate test
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0x50,
+            OP_SBC_IMM, 0xF0,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0x50,
+            OP_SBC_IMM, 0xB0,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0x50,
+            OP_SBC_IMM, 0x70,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0x50,
+            OP_SBC_IMM, 0x30,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0xD0,
+            OP_SBC_IMM, 0xF0,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0xD0,
+            OP_SBC_IMM, 0xB0,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0xD0,
+            OP_SBC_IMM, 0x70,
+
+            OP_SEC_IMP,
+            OP_LDA_IMM, 0xD0,
+            OP_SBC_IMM, 0x30,
+            
+            OP_STA_ZP, 0x78,
+            OP_DEC_ZP, 0x78,
+
+            OP_STA_ABS, 0x12, 0x34,
+            OP_DEC_ABS, 0x12, 0x34,
+
             OP_LDA_IMM, 0xAA,       // 2 cycles
+            OP_PHA_IMP,
+            OP_PLA_IMP,
+            OP_LDY_ABS, 0xFF, 0x01,
+            OP_LDY_ABS, 0xFE, 0x01,
+
             OP_STA_ZP,  0x00,       // 3 cycles
             OP_LDA_ZP,  0x00,       // 3 cycles
             OP_LDX_ZP,  0x00,       // 3 cycles
@@ -136,13 +189,14 @@ void demo_ram() {
             OP_LDY_ABS, 0x35, 0x12, // 4 cycles
 
             OP_CLC_IMP,
-            OP_BCC_REL, 0x02,
+            OP_BCS_REL, 0x02,
+            OP_RTS_IMP,
 
             OP_BRK_IMP,            // 7 cycles
-            OP_JMP_ABS, 0x00, 0x01, // 3 cycles
+            OP_JMP_ABS, 0x00, 0x03, // 3 cycles
         },
         .program_size = sizeof(test.program),
-        .program_address = 0x0100,
+        .program_address = 0x0300,
         .setup = [](cpu_state *cpu) {
             // Initial setup if needed
         },
@@ -152,7 +206,7 @@ void demo_ram() {
             assert(cpu->C == 0);
             assert(raw_memory_read(cpu, 0x0000) == 0xAA);
             assert(raw_memory_read(cpu, 0x1234) == 0xAA);
-            assert_cycles(cpu, 150);
+            assert_cycles(cpu, 219);
         }
     };
 
