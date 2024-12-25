b28226b5f4a6d1d7b4886379ec6523d096846ac8
Jawaid Bazyar <bazyar@gmail.com>
2024-12-25 16:04:17 -0500

    insert keyboard hooks

--- bus.cpp
+++ bus.cpp
@@ -3,6 +3,9 @@
 #include <sstream>
 #include "bus.hpp"
 
+#include "display/text_40x24.hpp"
+#include "devices/keyboard.hpp"
+
 /**
  * Process read and write to simulated IO bus for peripherals 
  * All external bus accesses are 8-bit data, 16-bit address.
@@ -10,10 +13,24 @@
  *
  * */
 
-void memory_bus_read(uint16_t address, uint8_t value) {
-
+uint8_t memory_bus_read(uint16_t address) {
+    if (address == KB_LATCH_ADDRESS) {
+        return kb_memory_read(address);
+    }
+    if (address == KB_CLEAR_LATCH_ADDRESS) {
+        return kb_memory_read(address);
+    }
+    return 0xEE; /* TODO: should return a random value 'floating bus'*/
 }
 
 void memory_bus_write(uint16_t address, uint8_t value) {
-
+    if (address >= 0x0400 && address <= 0x07FF) {
+        txt_memory_write(address, value);
+    }
+    if (address == KB_LATCH_ADDRESS) {
+        kb_memory_write(address, value);
+    }
+    if (address == KB_CLEAR_LATCH_ADDRESS) {
+        kb_memory_write(address, value);
+    }
 }
--- bus.hpp
+++ bus.hpp
@@ -2,5 +2,5 @@
 
 #include <cstdint>
 
-void memory_bus_read(uint16_t address, uint8_t value);
+uint8_t memory_bus_read(uint16_t address);
 void memory_bus_write(uint16_t address, uint8_t value);
