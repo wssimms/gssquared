9fb450de4fb6f385fc33f3b0be37b341055e2b9f
Jawaid Bazyar <bazyar@gmail.com>
2024-12-27 23:17:36 -0500

    add func decl

--- devices/keyboard.hpp
+++ devices/keyboard.hpp
@@ -10,3 +10,4 @@ uint8_t kb_memory_read(uint16_t address);
 void kb_memory_write(uint16_t address, uint8_t value);
 void kb_key_pressed(uint8_t key);
 void handle_sdl_keydown(cpu_state *cpu, SDL_Event event);
+void init_keyboard();
