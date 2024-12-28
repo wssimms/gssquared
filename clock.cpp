c7f07a73951309ad2c9901fbfcc01d392dc782bd
Jawaid Bazyar <bazyar@gmail.com>
2024-12-28 17:13:02 -0500

    tweak the clock handling to adjust towards a next time instead of dumb wait this time.

--- clock.cpp
+++ clock.cpp
@@ -6,12 +6,12 @@
 #include "clock.hpp"
 
 // nanosleep isn't real. Combine usleep and the busy wait.
-void emulate_clock_cycle(cpu_state *cpu) {
+void emulate_clock_cycle_old(cpu_state *cpu) {
     
     /* uint64_t start_time = mach_absolute_time();  // Get the start time */
     uint64_t sleep_time = cpu->cycle_duration_ticks;
     // Optional: Add a small sleep for longer intervals if necessary to reduce CPU usage
-    if (sleep_time < 25000) {
+    if (sleep_time < 25000) { // disable sleep for now.
         uint64_t current_time = mach_absolute_time();
         while (current_time - cpu->last_tick <= cpu->cycle_duration_ticks) {
             current_time = mach_absolute_time();
@@ -21,6 +21,29 @@ void emulate_clock_cycle(cpu_state *cpu) {
         nanosleep(&req, NULL);
     }
     cpu->last_tick = mach_absolute_time();
+}
+
+void emulate_clock_cycle(cpu_state *cpu) {
+    
+    uint64_t current_time = mach_absolute_time();
+    if (cpu->next_tick < current_time) { // clock slip
+        cpu->next_tick = current_time + cpu->cycle_duration_ticks;
+        cpu->clock_slip++;
+    } else {
+        uint64_t sleep_time = (cpu->next_tick - current_time);
+        if (sleep_time < 25000) { // if we're close, just busy wait
+            while ( current_time < cpu->next_tick) {
+                current_time = mach_absolute_time();
+            }
+            cpu->clock_busy++;
+        } else {
+            struct timespec req = {0, static_cast<long> (sleep_time ) };
+            nanosleep(&req, NULL);
+            cpu->clock_sleep++;
+        }
+        cpu->next_tick = cpu->next_tick + cpu->cycle_duration_ticks;
+    }
+    cpu->last_tick = current_time;
 }
 
 void incr_cycles(cpu_state *cpu) {
