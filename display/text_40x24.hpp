db128d85b6435c680aaf5d8de4a42c66be7615f1
Jawaid Bazyar <bazyar@gmail.com>
2024-12-27 23:14:55 -0500

    clean up includes

--- display/text_40x24.hpp
+++ display/text_40x24.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
-#include "../gs2.hpp"
 #include "../cpu.hpp"
-#include "../types.hpp"
 
 uint64_t init_display();
 void free_display();
 void txt_memory_write(uint16_t , uint8_t );
 void update_display(cpu_state *cpu);
 void update_flash_state(cpu_state *cpu);
+void init_text_40x24_display();
