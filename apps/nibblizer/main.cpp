f2d4b2bc4c0a0a484f071cd59c2d8d636dc15b18
Jawaid Bazyar <bazyar@gmail.com>
2025-01-10 18:37:28 -0500

    add copyright notices

--- apps/nibblizer/main.cpp
+++ apps/nibblizer/main.cpp
@@ -1,3 +1,20 @@
+/*
+ *   Copyright (c) 2025 Jawaid Bazyar
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <unistd.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -12,7 +29,7 @@
 
 int main() {
 
-    disk_t disk = { };       // start with zeroed disk.
+    nibblized_disk_t disk = { };       // start with zeroed disk.
     memcpy(disk.interleave_phys_to_logical, do_phys_to_logical, sizeof(interleave_t));
     memcpy(disk.interleave_logical_to_phys, do_logical_to_phys, sizeof(interleave_t));
     
