e43666845632ecc1a775cdd57d891110520c828b
Jawaid Bazyar <bazyar@gmail.com>
2025-01-11 12:33:51 -0500

    experiment with sdl dialogs for error reporting

---
+++ src/util/dialog.cpp
@@ -0,0 +1,28 @@
+/*
+ *   Copyright (c) 2025 Jawaid Bazyar
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+void system_failure(const char *message) {
+    printf("Error: %s\n", message);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", message, NULL);
+}
+
+void system_diag(char *message) {
+    printf("Information: %s\n", message);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Information", message, NULL);
+}
