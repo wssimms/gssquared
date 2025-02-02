9a5fc43a9c8bfbb5d2d570dd976fb1eac609528a
Jawaid Bazyar <bazyar@gmail.com>
2025-02-01 20:58:37 -0500

    take filenames by command-line arg

--- apps/nibblizer/main.cpp
+++ apps/nibblizer/main.cpp
@@ -27,7 +27,38 @@
  * for now, read only!
  */
 
-int main() {
+void print_usage(const char* program_name) {
+    fprintf(stderr, "Usage: %s [-v] [-o output.nib] input_file\n", program_name);
+    fprintf(stderr, "  -o filename    Write output to filename (default: output.nib)\n");
+    fprintf(stderr, "  -v            Verbose mode - dump disk information\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    const char* output_filename = "output.nib";
+    const char* input_filename = nullptr;
+    bool verbose = false;
+    int opt;
+
+    // Process command line options
+    while ((opt = getopt(argc, argv, "o:v")) != -1) {
+        switch (opt) {
+            case 'o':
+                output_filename = optarg;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            default:
+                print_usage(argv[0]);
+        }
+    }
+
+    // Get input filename (first non-option argument)
+    if (optind >= argc) {
+        print_usage(argv[0]);
+    }
+    input_filename = argv[optind];
 
     nibblized_disk_t disk = { };       // start with zeroed disk.
     memcpy(disk.interleave_phys_to_logical, do_phys_to_logical, sizeof(interleave_t));
@@ -36,16 +67,20 @@ int main() {
     sector_t sectors[16];
     disk_image_t disk_image;
 
-    int ret = load_disk_image(disk_image, "disk_sample.do");
+    int ret = load_disk_image(disk_image, input_filename);
     if (ret < 0) {
-        printf("Failed to load disk image\n");
+        fprintf(stderr, "Failed to load disk image: %s\n", input_filename);
         exit(1);
     }
-    dump_disk_image(disk_image);
+    if (verbose) {
+        dump_disk_image(disk_image);
+    }
 
     emit_disk(disk, disk_image, DEFAULT_VOLUME);
-    write_disk(disk, "disk_sample.nib");
+    write_disk(disk, output_filename);
 
-    dump_disk(disk);
-
+    if (verbose) {
+        dump_disk(disk);
+    }
+    return 0;
 }
