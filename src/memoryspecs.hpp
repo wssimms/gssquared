49f1de2cbff3e8c2e74066e817d75d049027575c
Jawaid Bazyar <bazyar@gmail.com>
2025-02-15 23:53:01 +0000

    ram must be 64KB - forgot to update size after ram map changes #1 #2

--- src/memoryspecs.hpp
+++ src/memoryspecs.hpp
@@ -21,7 +21,7 @@
 #define RAM_MB 1
 #define RAM_SIZE RAM_MB * 1024 * 1024
 #else
-#define RAM_KB 48 * 1024
+#define RAM_KB 64 * 1024
 #define IO_KB 4 * 1024
 #define ROM_KB 12 * 1024
 #define MEMORY_SIZE 64 * 1024
